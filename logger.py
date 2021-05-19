be33de6d6b3fb1f6ee91ffa90abd559101dc78e7
awol <awollinux@gmail.com>
2021-05-18 23:20:05 -0400

    added type hints

--- logger.py
+++ logger.py
@@ -1,24 +1,22 @@
 import os
-from discord.ext import commands
 import discord
+from discord.ext import commands
 import mongo
 import datetime
-import typing
+from typing import Optional, Union
 
 
 def Diff(li1, li2):
     """
     Give two lists and return the diffrances
     """
-    li_dif = [i for i in li1 + li2 if i not in li1 or i not in li2]
-    return li_dif
+    return [i for i in li1 + li2 if i not in li1 or i not in li2]
 
 
 def setup(bot):
     bot.add_cog(public_logger(bot))
 
 
-# main function
 class public_logger(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -26,7 +24,9 @@ class public_logger(commands.Cog):
         self.log_handlers = log_handlers(self.bot)
 
     @commands.Cog.listener()
-    async def on_member_update(self, before, after):
+    async def on_member_update(
+        self, before: discord.Member, after: discord.Member
+    ) -> bool:
         """
         If role is mute then log mute
         Else log the role change
@@ -62,7 +62,7 @@ class public_logger(commands.Cog):
             action=action,
         )
 
-        await self.log_handlers.log_event(
+        return await self.log_handlers.log_event(
             action=action,
             entry=entry,
             role=role,
@@ -71,7 +71,13 @@ class public_logger(commands.Cog):
         )
 
     @commands.Cog.listener()
-    async def on_voice_state_update(self, member, before, after):
+    async def on_voice_state_update(
+        self,
+        member: discord.Member,
+        before: discord.VoiceState,
+        after: discord.VoiceState,
+    ) -> bool:
+
         if before.mute != after.mute:
             action = "Toggled_VC_Mute"
             if before.mute is False:
@@ -88,28 +94,25 @@ class public_logger(commands.Cog):
                 action_pretty = "VC undeafened"
             log_action = discord.AuditLogAction.member_update
 
-        ## discord doesn't target so wauting for privat log
-        #        elif before.channel != None and after.channel == None:
-        #            action = 'Kicked_User_From_VC'
-        #            log_action = discord.AuditLogAction.member_disconnect
-
         else:
             return True
         self.settings = mongo.settings(member.guild)
         entry = await self.log_handlers.get_audit_log_event(
             log_action=log_action, guild=member.guild, action=action
         )
-        if entry is not None:
-            await self.log_handlers.log_event(
-                action=action,
-                entry=entry,
-                guild=member.guild,
-                target=member,
-                action_pretty=action_pretty,
-            )
+        if entry is None:
+            return False
+
+        return await self.log_handlers.log_event(
+            action=action,
+            entry=entry,
+            guild=member.guild,
+            target=member,
+            action_pretty=action_pretty,
+        )
 
     @commands.Cog.listener()
-    async def on_member_ban(self, guild, user: discord.User):
+    async def on_member_ban(self, guild: discord.Guild, user: discord.User) -> bool:
         """
         Log ban events
         """
@@ -118,10 +121,12 @@ class public_logger(commands.Cog):
         entry = await self.log_handlers.get_audit_log_event(
             log_action=log_action, guild=guild, user=user, action=action
         )
-        await self.log_handlers.log_event(action=action, entry=entry, guild=guild)
+        return await self.log_handlers.log_event(
+            action=action, entry=entry, guild=guild
+        )
 
     @commands.Cog.listener()
-    async def on_member_unban(self, guild, user: discord.User):
+    async def on_member_unban(self, guild: discord.Guild, user: discord.User) -> bool:
         """
         Log ban events
         """
@@ -130,7 +135,9 @@ class public_logger(commands.Cog):
         entry = await self.log_handlers.get_audit_log_event(
             log_action=log_action, guild=guild, user=user, action=action
         )
-        await self.log_handlers.log_event(action=action, entry=entry, guild=guild)
+        return await self.log_handlers.log_event(
+            action=action, entry=entry, guild=guild
+        )
 
 
 class log_handlers(object):
@@ -139,13 +146,14 @@ class log_handlers(object):
 
     async def log_event(
         self,
-        action=None,
-        entry=None,
-        guild=None,
-        role=None,
-        target=None,
-        action_pretty=None,
-    ):
+        entry: Optional[discord.AuditLogEntry] = None,
+        guild: Optional[discord.Guild] = None,
+        role: Optional[discord.Role] = None,
+        target: Optional[Union[discord.User, discord.Member]] = None,
+        action: Optional[str] = None,
+        action_pretty: Optional[str] = None,
+    ) -> bool:
+
         """
         Send event into moderation log
         """
@@ -156,9 +164,7 @@ class log_handlers(object):
         if action_pretty == None:
             action_pretty = action
 
-        print(action_pretty)
-
-        # dont log bots because they log themselves
+        # Dont log bots because they log themselves
         if entry.user.bot or entry.user == entry.target:
             return
 
@@ -201,25 +207,33 @@ class log_handlers(object):
 
         # send log
         log_channel = self.log_location(action, guild)
-        if log_channel is not None:
-            log = await self.bot.fetch_channel(log_channel)
-            await log.send(embed=embedVar)
+        if log_channel is None:
+            return False
+
+        log = await self.bot.fetch_channel(log_channel)
+        return await log.send(embed=embedVar)
 
     async def get_audit_log_event(
-        self, log_action=None, guild=None, action=None, user=None
-    ):
+        self,
+        log_action: Optional[discord.AuditLogAction] = None,
+        guild: Optional[discord.Guild] = None,
+        action: Optional[str] = None,
+        action_pretty: Optional[str] = None,
+        user: Optional[Union[discord.User, discord.Member]] = None,
+    ) -> discord.AuditLogEntry:
         """
         Get audit log events that match action and target if target exists
         """
-        async for entry in guild.audit_logs(limit=1):
-            print(entry)
+        audit_list = await guild.audit_logs(limit=10).flatten()
+        for entry in audit_list:
+            print(f"Action: {log_action} Entry action: {entry.action}")
             if entry.action == log_action:
                 if entry.target == user or user == None:
+                    print(
+                        f"Action: {entry.action}, User: {entry.user.nick}, Target: {entry.target}. Is user bot: {entry.user.bot}, Guild: {entry.guild}"
+                    )
                     return entry
 
-    def log_location(self, log_item, guild):
+    def log_location(self, log_item: str, guild: discord.Guild) -> int:
         self.settings = mongo.settings(guild)
-        channel_id = self.settings.get(log_item)
-        if channel_id is None:
-            channel_id = self.settings.get("log_channel_id")
-        return channel_id
+        return self.settings.get(log_item) or self.settings.get("log_channel_id")
